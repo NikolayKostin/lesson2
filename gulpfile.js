9afaea515f193c983e53f24b2917f70f8fc56781
Nik <nikolaymetos@gmail.com>
2017-04-02 19:28:17 +0300

    2nd commit

--- gulpfile.js
+++ gulpfile.js
@@ -4,27 +4,29 @@ var clean = require('gulp-clean');
 var googlecdn = require('gulp-google-cdn');
  
 gulp.task('default', function () {
-    return gulp.src('index.html')
-        .pipe(googlecdn(require('./bower.json')))
-        .pipe(gulp.dest('dist'));
+    return gulp.src('./index.html')
+        .pipe(googlecdn(require('./bower.json'), {
+        	componentsPath: './bower_components/'
+        }))
+        .pipe(gulp.dest('./dist'));
+        done();
 });
 
-
 gulp.task('clean', function (done) {
-    return gulp.src('./dist/css', {read: false})
+    return gulp.src('./dist', {read: false})
         .pipe(clean());
 done();
 });
 
-gulp.task('scss', gulp.series('clean', function (done) {
+gulp.task('scss', gulp.series('clean', 'default', function (done) {
   return gulp.src('./scss/**/*.scss')
     .pipe(sass({outputStyle: 'expanded'}).on('error', sass.logError))
-    .pipe(gulp.dest('dist/css'));
+    .pipe(gulp.dest('./dist/css'));
 done();
 }));
 
 
 
 gulp.task('scss:watch', function () {
-  gulp.watch('./scss/**/*.scss', gulp.series('clean','scss'));
+  gulp.watch('./scss/**/*.scss', gulp.series('scss', 'default'));
 });
